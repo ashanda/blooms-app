096abb59b4151db6fe73f0a8d07288691b03061f
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-05-14 18:40:51 +0530

    modification

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
   <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
   <a class="navbar-brand m-0" href="https://bloomskin.clinic" target="_blank">
     <img src="{{ asset('img/bloom-skin-clinic-logo.png')}}" class="navbar-brand-img h-100" alt="main_logo">
-    <span class="ms-1 font-weight-bold">{{  env('APP_NAME') }}</span>
+    <span class="ms-1 font-weight-bold">{{  env('APP_NAME') }} </span>
   </a>
 </div>
 <hr class="horizontal dark mt-0">
